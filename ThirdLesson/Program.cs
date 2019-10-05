ea99327e47e9e9e6d4a627bf41529025532aec37
Paweł Skalski <40362366+pawelskalski@users.noreply.github.com>
2019-10-05 14:07:58 +0200

    First commit

---
+++ ThirdLesson/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ThirdLesson
+{
+    class Program
+    {
+        static float studentAverage(List<int> grades)
+        {
+            float average;
+            average = (float) grades.Sum() / (float) grades.Count;
+            return average;
+        }
+
+        static void Main(string[] args)
+        {
+            List<int> grades = new List<int>();
+            int grade = 0;
+            while (grade != -1)
+            {
+                try
+                {
+                    grade = Convert.ToInt32(Console.ReadLine());
+                    if (grade > 6)
+                    {
+                        Console.WriteLine("Ocena nie moze byc wyzsza niz 6");
+                        continue;
+                    }
+
+                    if (grade < 1 && grade != -1)
+                    {
+                        Console.WriteLine("Ocena nie może być mniejsza niż 1");
+                        continue;
+                    }
+
+                    if (grade != -1)
+                    {
+                        grades.Add(grade);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("To nie jest liczba");
+                    continue;
+                }
+            }
+            Console.WriteLine(studentAverage(grades));
+        }
+    }
+}
